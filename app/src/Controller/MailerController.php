ddba3f6b103201a21abad603386ff683bbc5339b
ikarolaborda <iclaborda@msn.com>
2022-03-13 20:54:02 -0300

    feat: add new form module for mail sending purposes

--- app/src/Controller/MailerController.php
+++ app/src/Controller/MailerController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
@@ -13,15 +17,39 @@ class MailerController extends AbstractController
     /**
      * @Route("/mailer", name="mail")
      */
-    public function sendEmail(MailerInterface $mailer): Response
+    public function sendEmail(MailerInterface $mailer, Request $request): Response
     {
-        $email = (new Email())
-            ->from('mesa01@digitalmenu.wip')
-            ->to('waiter@digitalmenu.wip')
-            ->subject('Pedido')
-            ->text('Extra fries');
+        $emailForm = $this->createFormBuilder()
+            ->add('message',TextareaType::class,['attr' => ['rows' => '5'], 'label' => 'Escreva sua Mensagem'])
+            ->add('sendMail',SubmitType::class,['label' => 'Enviar Mensagem'])
+            ->getForm();
 
-        $mailer->send($email);
-        return new Response('email enviado');
+        $emailForm->handleRequest($request);
+
+        if($emailForm->isSubmitted()) {
+            $input = $emailForm->getData();
+
+            $table = 'Mesa 1';
+            $text = $input['message'];
+            $email = (new TemplatedEmail())
+                ->from('mesa01@digitalmenu.wip')
+                ->to('waiter@digitalmenu.wip')
+                ->subject('Pedido')
+
+                ->htmlTemplate('mailer/mailer.html.twig')
+
+                ->context([
+                    'table' => $table,
+                    'text' => $text
+                ]);
+
+            $mailer->send($email);
+            $this->addFlash('email_message_sent','Sua mensagem foi Enviada!');
+            return $this->redirect($this->generateUrl('mail'));
+        }
+
+        return $this->render('mailer/index.html.twig', [
+            'emailForm' => $emailForm->createView()
+        ]);
     }
 }
